01881a7c7fe1b47bbcb76e522674ee028b53223c
jpda <john.dandison@live.com>
2019-11-21 14:05:00 -0500

    issuer validation

---
+++ MultitenantB2C.OpenId/AzureAdOptions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultitenantB2C.OpenId
+{
+    public class AzureAdOptions
+    {
+        public const string AuthenticationScheme = "AzureAd";
+    }
+
+    public class AzureAdB2COptions
+    {
+        public const string AuthenticationScheme = "AzureAdB2C";
+    }
+
+    public class AzureAdChinaOptions
+    {
+        public const string AuthenticationScheme = "AzureAdChina";
+    }
+
+    public class AzureAdGovOptions
+    {
+        public const string AuthenticationScheme = "AzureAdGov";
+    }
+
+    public class AzureAdGermanyOptions
+    {
+        public const string AuthenticationScheme = "AzureAdGermany";
+    }
+}
---
+++ MultitenantB2C.OpenId/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultitenantB2C.OpenId.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthenticationSchemeProvider _provider;
+
+        public AuthController(IAuthenticationSchemeProvider schemeProvider)
+        {
+            _provider = schemeProvider;
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _provider.GetAllSchemesAsync());
+        }
+
+        [Authorize(AuthenticationSchemes = AzureAdOptions.AuthenticationScheme)]
+        public IActionResult AzureAd()
+        {
+            return Redirect("/");
+        }
+
+        [Authorize(AuthenticationSchemes = AzureAdB2COptions.AuthenticationScheme)]
+        public IActionResult AzureAdB2C()
+        {
+            return Redirect("/");
+        }
+    }
+}
