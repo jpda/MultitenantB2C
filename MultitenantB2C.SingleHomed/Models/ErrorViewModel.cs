b40c3c72c453c01fdf9fd53bcbba37e4b3d7f21b
jpda <john.dandison@live.com>
2019-11-21 15:20:28 -0500

    added b2c policies

---
+++ MultitenantB2C.SingleHomed/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MultitenantB2C.SingleHomed.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
